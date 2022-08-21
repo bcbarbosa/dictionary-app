c171079684f80ac494ff6a1bcc63683a0fc69d4a
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-21 10:33:18 +0100

    Update Dictionary.js
    Added axios
    Added search function
    Added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,42 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css"
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response);
+    }
+
     function search(event) {
         event.preventDefault();
-        alert(`We are searching here... your word was ${keyword} right?`);
+        alert(`searching for ${keyword} definition`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
+
+    // function handleSubmit(event) {
+    //     event.preventDefault();
+    //     search();
+    // }
+
+    // function search() {
+    //     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    //     axios
+    //         .get(apiUrl)
+    //         .then(response => {
+    //             const { word } = response.data;
+    //             setKeyword({
+    //                 ready: true,
+    //                 word: word
+    //             });
+    //         })
+    //         .catch(error => {
+    //             console.log("Error fetching data, ", error);
+    //         });
+    // }
 
     function handleKeyWordChange(event) {
         setKeyword(event.target.value);
@@ -33,6 +62,9 @@ export default function Dictionary() {
                             className="btn btn-primary w-100" />
                     </div></div>
             </form>
+            <div className="meaning">
+                {keyword.word}
+            </div>
         </div>
     )
 }
